46a8118366491b90c70c2cbf0cd21651859e0ead
Adam Parkin <aparkin@seekersolutions.com>
2013-05-23 14:04:16 -0700

    Works, but slooooow

--- src/main/java/com/seeker/anagramkata/RunMe.java
+++ src/main/java/com/seeker/anagramkata/RunMe.java
@@ -2,10 +2,15 @@ package com.seeker.anagramkata;
 
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.primitives.Chars;
 
 public class RunMe {
     public static List<String> openWordList() {
@@ -22,10 +27,43 @@ public class RunMe {
         System.out.println(theAnswer());
         
         List<String> lines = openWordList();
-        // print out 1st 10 lines
-        for (int x = 0; x < 10; x++) {
-            System.out.println(lines.get(x));
+        lines = Arrays.asList("no", "on", "bo");
+        Set<Set<String>> anagrams = findAnagramsInList(lines);
+        for (Set<String> group : anagrams) {
+            System.out.println(group);
         }
+    }
+
+    public static Set<Set<String>> findAnagramsInList(final List<String> inputIn) {
+        Set<Set<String>> result = new HashSet<Set<String>>();
+        
+        for (String word : inputIn) {
+            boolean groupFound = false;
+            for (Set<String> group : result) {
+                String groupWord = group.iterator().next();
+                if (isAnagram(groupWord, word)) {
+                    group.add(word);
+                    groupFound = true;
+                    break;
+                }
+            }
+            if (!groupFound) {
+                result.add(new HashSet<String>(Arrays.asList(word)));
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isAnagram(final String word1, final String word2) {
+        if (word1.length() == word2.length()) {
+            List<Character> word1list = Chars.asList(word1.toCharArray());
+            Collections.sort(word1list);
+            List<Character> word2list = Chars.asList(word2.toCharArray());
+            Collections.sort(word2list);
+            return word1list.equals(word2list);
+        }
+        return false;
     }
 }
 
--- src/test/java/com/seeker/anagramkata/TestRunMe.java
+++ src/test/java/com/seeker/anagramkata/TestRunMe.java
@@ -3,9 +3,13 @@ package com.seeker.anagramkata;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
+
+import com.google.common.collect.Sets;
 
 
 public class TestRunMe {
@@ -17,4 +21,44 @@ public class TestRunMe {
         assertEquals(expected, result);
     }
 
+    @Test
+    public void findAnagramsInListFindsAllAnagramsWithSmallInput() {
+        List<String> input = Arrays.asList("no", "on", "bo");
+        Set<Set<String>> expected = new HashSet<Set<String>>();
+        expected.add(Sets.newHashSet(Arrays.asList("no", "on")));
+        expected.add(Sets.newHashSet(Arrays.asList("bo")));
+        
+        Set<Set<String>> result = RunMe.findAnagramsInList(input);
+        
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void findAnagramsInListFindsAllAnagramsWithSmallNonOverlappingInput() {
+        List<String> input = Arrays.asList("no", "bo");
+        Set<Set<String>> expected = new HashSet<Set<String>>();
+        expected.add(Sets.newHashSet(Arrays.asList("no")));
+        expected.add(Sets.newHashSet(Arrays.asList("bo")));
+        
+        Set<Set<String>> result = RunMe.findAnagramsInList(input);
+        
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void isAnagramWithAnagramReturnsTrue() {
+        String word1 = "rats";
+        String word2 = "star";
+        
+        assertTrue(RunMe.isAnagram(word1, word2));
+    }
+    
+    @Test
+    public void isAnagramWithAnagramReturnsFalse() {
+        String word1 = "false";
+        String word2 = "star";
+        
+        assertFalse(RunMe.isAnagram(word1, word2));
+        
+    }
 }
