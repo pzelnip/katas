e9c2f9f8f67c4c8019c573c2e5324206be9d74e0
Adam Parkin <aparkin@seekersolutions.com>
2013-05-24 21:41:57 -0700

    Instrumenting the code to figure out why my results are whacked

--- src/main/java/com/seeker/anagramkata/RunMe.java
+++ src/main/java/com/seeker/anagramkata/RunMe.java
@@ -1,13 +1,8 @@
 package com.seeker.anagramkata;
 
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class RunMe {
 
@@ -16,17 +11,41 @@ public class RunMe {
         String result = new Scanner(istream).useDelimiter("\\A").next();
         return Arrays.asList(result.split("\\n"));
     }
+    
+    public static List<String> toLower(final List<String> in) {
+        List<String> result = new ArrayList<String>();
+        for (String s : in) {
+            result.add(s.toLowerCase());
+        }
+        return result;
+    }
 
     public static void main(final String[] args) {
-        List<String> lines = openWordList();
+        List<String> lines = toLower(openWordList());
         System.out.println("There are " + lines.size() + " words");
         Set<Set<String>> anagrams = findAnagramsInList(lines);
         System.out.println("Found " + anagrams.size() + " groups of anagrams");
         for (Set<String> group : anagrams) {
             System.out.println(group);
         }
+        System.out.println(examineCounts);
+        long total = 0;
+        for (Entry<String,Integer> entry : examineCounts.entrySet()){
+            total += entry.getValue();
+        }
+        System.out.println("Total examinations: " + total);
     }
 
+    static Map<String,Integer> examineCounts = new HashMap<String,Integer>();
+    private static void incrementCount(final String word) {
+        if (examineCounts.containsKey(word)) {
+            examineCounts.put(word, examineCounts.get(word) + 1);
+        } else {
+            examineCounts.put(word, 1);
+        }
+    }
+    
+    
     public static Set<Set<String>> findAnagramsInList(final List<String> inputIn) {
         Set<Set<String>> result = new HashSet<Set<String>>();
         Queue<String> inputq = new LinkedList<String>(inputIn);
@@ -35,10 +54,12 @@ public class RunMe {
             String word = inputq.remove();
             Set<String> group = new HashSet<String>();
             group.add(word);
+//incrementCount(word);
 
             Queue<String> dupe = new LinkedList<String>();
             while (inputq.size() != 0) {
                 String possible = inputq.remove();
+incrementCount(word);
                 if (isAnagram(possible, word)) {
                     group.add(possible);
                 } else {
