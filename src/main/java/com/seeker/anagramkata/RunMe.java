d36d1def0aa411af70a5c752d4da6da39ba0ac36
Adam Parkin <aparkin@seekersolutions.com>
2013-05-25 08:55:00 -0700

    Added some code for spitting out metrics about dataset

--- src/main/java/com/seeker/anagramkata/RunMe.java
+++ src/main/java/com/seeker/anagramkata/RunMe.java
@@ -19,9 +19,65 @@ public class RunMe {
         return Arrays.asList(result.split("\\n"));
     }
 
+    static void spitStats(final List<String> words) {
+        long total = 0;
+        int min = Integer.MAX_VALUE;
+        String minword = "";
+        int max = 0;
+        String maxword = "";
+        Map<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
+        for (String word : words) {
+            int wordlen = word.length();
+
+            Integer count = frequencies.get(wordlen);
+            if (count == null) {
+                frequencies.put(wordlen, 1);
+            } else {
+                frequencies.put(wordlen, count + 1);
+            }
+            total += wordlen;
+            if (wordlen < min) {
+                min = wordlen;
+                minword = word;
+            }
+            if (wordlen > max) {
+                max = wordlen;
+                maxword = word;
+            }
+        }
+
+        double avglen = (total * 1.0) / words.size();
+        System.out.println("There are " + words.size() + " words");
+        System.out.println("Average length: " + avglen);
+        System.out.println(String.format("Max length: %s (%s)", maxword, max));
+        System.out.println(String.format("Min length: %s (%s)", minword, min));
+        System.out.println("Histogram:");
+        for (int x = min; x <= max; x++) {
+            Integer count = frequencies.get(x);
+            System.out.print(String.format("%2d: ", x));
+            if (count != null) {
+                for (int y = 0; y < count / 80; y++) {
+                    System.out.print("X");
+                }
+                if (count / 80 < 1 && count != 0) {
+                    System.out.print("X");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.print("Histogram raw counts: ");
+        for (int x = min; x <= max; x++) {
+            Integer count = frequencies.get(x);
+            System.out.print(String
+                    .format("%s ", (count == null ? "0" : count)));
+        }
+        System.out.println();
+    }
+
     public static void main(final String[] args) {
         List<String> lines = openWordList();
-        System.out.println("There are " + lines.size() + " words");
+        spitStats(lines);
         Collection<Set<String>> anagrams = findAnagramsInList(lines);
         System.out.println("Found " + anagrams.size() + " groups of anagrams");
         for (Set<String> group : anagrams) {
@@ -29,8 +85,7 @@ public class RunMe {
         }
     }
 
-    public static Set<Set<String>> findAnagramsInList(
-            final List<String> inputIn) {
+    public static Set<Set<String>> findAnagramsInList(final List<String> inputIn) {
         Map<String, Set<String>> anagrams = new HashMap<String, Set<String>>();
 
         for (String s : inputIn) {
