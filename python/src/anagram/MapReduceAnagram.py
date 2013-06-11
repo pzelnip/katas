2cdb133f546ee88f427120f28883b4797ebc6668
Adam Parkin <aparkin@seekersolutions.com>
2013-06-11 08:25:27 -0700

    Added basic Python solution to anagram finder kata

--- python/src/anagram/MapReduceAnagram.py
+++ python/src/anagram/MapReduceAnagram.py
@@ -1,4 +1,7 @@
 '''
+Solve the anagram finding problem via map/reduce.  The idea here is to have all
+words which are anagrams of each other get mapped to the same reduce bucket. 
+
 Created on Jun 10, 2013
 
 @author: aparkin
@@ -30,7 +33,7 @@ class MapReduce:
                 record = json.loads(line)
                 mapper(record)
             except ValueError:
-                print "Bad line, skipping: '%s'" % line
+                print("Bad line, skipping: '%s'" % line)
 
         for key in self.intermediate:
             reducer(key, self.intermediate[key])
@@ -38,11 +41,6 @@ class MapReduce:
         jenc = json.JSONEncoder()
         return jenc.encode(self.result)
 
-
-'''
-Solve the anagram finding problem via map/reduce.  The idea here is to have all
-words which are anagrams of each other get mapped to the same reduce bucket. 
-'''
 
 mr = MapReduce()
 
@@ -59,7 +57,7 @@ def reducer(key, values):
 def main():
     inputdata = open(sys.argv[1])
     json_result = mr.execute(inputdata, mapper, reducer)
-    print json.dumps(json.loads(json_result), sort_keys = True, indent = 2)
+    print (json.dumps(json.loads(json_result), sort_keys = True, indent = 2))
 
 
 if __name__ == "__main__":
